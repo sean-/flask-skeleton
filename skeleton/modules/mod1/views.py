46dd4064b8479d5fd4dc7dbdbe659bea143a51c2
Sean Chittenden <sean@chittenden.org>
2011-05-19 15:33:54 -0700

    Add a parameter, id, to list_one

--- skeleton/modules/mod1/views.py
+++ skeleton/modules/mod1/views.py
@@ -1,12 +1,18 @@
-from flask import g, render_template
+from flask import render_template, request
 from mod1 import module
-from mod1.models import H1
+from mod1.models import H2
 
 @module.route('/mod1_view')
 def index(name = None):
     return render_template('mod1/mod1_view.html', name=name)
 
-@module.route('/list')
-def list():
-    entries = H1.select().execute()
-    return render_template('mod1/list.html', entries=entries)
+@module.route('/list_all')
+def list_all():
+    entries = H2.query.all()
+    return render_template('mod1/list_all.html', entries=entries)
+
+@module.route('/list_one')
+def list_one():
+    id = request.args.get('id', 1)
+    entry = H2.query.filter_by(id=id).first_or_404()
+    return render_template('mod1/list_one.html', entry=entry)
