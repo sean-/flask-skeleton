15a48c6493440824738631c02121f67085d7fc9b
Sean Chittenden <sean@chittenden.org>
2011-05-19 23:11:21 -0700

    Nuke the useless parameter 'name' from home/index

--- skeleton/modules/home/views.py
+++ skeleton/modules/home/views.py
@@ -2,5 +2,5 @@ from flask import render_template
 from home import module
 
 @module.route('/')
-def index(name = None):
-    return render_template('home/index.html', name = name)
+def index():
+    return render_template('home/index.html')
