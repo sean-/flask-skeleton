3575cf533793f6f02c8077d30510d7eb99a80666
Sean Chittenden <sean@chittenden.org>
2011-05-23 12:09:55 -0700

    Nuke the create_db script. I didn't realize I'd actually committed that.

--- create_db.py
+++
@@ -1,8 +0,0 @@
-from skeleton import app
-from skeleton.database import create_db
-
-if __name__ == '__main__':
-    create_db(schema_name=app.config['DB_SCHEMA'],
-              admin_name=app.config['DB_ADMIN'],
-              user_name=app.config['DB_USER'])
-
