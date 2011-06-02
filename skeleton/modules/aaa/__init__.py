bf8431576d00a987ab0d1e8f7243b9d0ffc645f4
Sean Chittenden <sean@chittenden.org>
2011-06-02 11:08:20 -0700

    Add 'fixup_destination_url()' as a way of making sure that users are always returned to the correct page. This requires cooperation in the use of parameters and session variable names. Currently I'm using 'dsturl' when requesting a return redirect.
    Add a few checks for local sessions vs remote sessions.
    
    Also moved gen_session_id() in to the aaa module.
    
    Signed-off-by: Sean Chittenden <sean@chittenden.org>

--- skeleton/modules/aaa/__init__.py
+++ skeleton/modules/aaa/__init__.py
@@ -1,3 +1,31 @@
-from flask import Module
+# -*- coding: utf-8 -*-
+import base64, hashlib, M2Crypto
+from functools import wraps
+
+from flask import current_app, flash, g, Markup, Module, redirect, request, session, url_for
 
 module = Module(__name__, 'aaa')
+
+LOGIN_SUFFIX_BLACKLIST = {
+    '/logout': True,
+    '/login': True
+}
+
+def gen_session_id():
+    """ Generates a session ID """
+    # Be kind to future support people and developers by using a base32
+    # encoded session id. Why is this cool? Read RFC3548 §5 and rejoice
+    # at the lack of ambiguity regarding "one", "ell", "zero" and
+    # "ohh". You can thank me later.
+    return base64.b32encode(M2Crypto.m2.rand_bytes(current_app.config['SESSION_BYTES']))
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'li' not in session:
+            session['dsturl'] = request.path
+            flash('Login required for previous page')
+            return redirect(url_for('aaa.login'))
+        return f(*args, **kwargs)
+    return decorated_function
+
