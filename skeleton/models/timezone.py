6ceb4ae67dc468a45f218e1dc19cc6a3b219240d
Sean Chittenden <sean@chittenden.org>
2011-06-09 12:42:24 -0700

    For better or worse, coopt the skeleton/models directory for "site global" data. This irks be something fierce. I may move this to "global" in just a few minutes it ticks off my sense of tab-completion so bad.
    Signed-off-by: Sean Chittenden <sean@chittenden.org>

---
+++ skeleton/models/timezone.py
@@ -0,0 +1,8 @@
+from skeleton import db
+
+class Timezone(db.Model):
+    __table__ = db.Table(
+        'timezone', db.metadata,
+        db.Column('id', db.Integer, primary_key=True),
+        db.Column('name', db.String),
+        schema='public')
