64ca47cd38ee6616b6c63b80414bc9e9e475979b
Sean Chittenden <sean@chittenden.org>
2011-06-09 20:22:55 -0700

    Add the trigger to shadow.aaa_email_confirmation_log().
    Signed-off-by: Sean Chittenden <sean@chittenden.org>

--- sql/initialize/400_triggers.sql
+++ sql/initialize/400_triggers.sql
@@ -1,4 +1,7 @@
 -- env PGDATABASE=skeleton PGUSER=skeleton_root
+CREATE TRIGGER aaa_email_confirmation_log_upd_trg BEFORE UPDATE ON shadow.aaa_email_confirmation_log
+  FOR EACH ROW EXECUTE PROCEDURE shadow.aaa_email_confirmation_log_upd_trg();
+
 CREATE TRIGGER aaa_session_expire_excess_trg BEFORE INSERT ON shadow.aaa_session
   FOR EACH ROW EXECUTE PROCEDURE shadow.aaa_session_expire_excess_trg();
 
