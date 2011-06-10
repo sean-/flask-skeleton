cb346c0018377c266d162194d32b0d1830fde2b3
Sean Chittenden <sean@chittenden.org>
2011-06-09 20:20:24 -0700

    Fixup changing the ownership of the aaa.login() now that login() accepts an extra bool for secure cookies.
    Signed-off-by: Sean Chittenden <sean@chittenden.org>

--- sql/initialize/500_fixup_func_owner.sql
+++ sql/initialize/500_fixup_func_owner.sql
@@ -2,8 +2,8 @@
 
 -- Change the owner of these functions to skeleton_shadow because SECURITY
 -- DEFINER is a set attribute for these functions. This must be run as the
--- pgsql user (can't be skeleton_dba because skeleton_dba isn't in the role
--- 'skeleton_shadow').
+-- pgsql user. This can't be run as skeleton_root because skeleton_root isn't
+-- a member of the role 'skeleton_shadow' (and shouldn't be!!!).
 ALTER FUNCTION aaa.expire_session(TEXT) OWNER TO skeleton_shadow;
-ALTER FUNCTION aaa.login(TEXT, BYTEA, INET, TEXT, INTERVAL) OWNER TO skeleton_shadow;
+ALTER FUNCTION aaa.login(TEXT, BYTEA, INET, TEXT, INTERVAL, BOOL) OWNER TO skeleton_shadow;
 ALTER FUNCTION aaa.register(TEXT, BYTEA, INET) OWNER TO skeleton_shadow;
